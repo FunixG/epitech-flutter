1a512d94f601fdd31c504b9d394e7e21f0ff289b
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-04-08 11:33:36 +0200

    removed unit tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,6 @@
-import 'package:epitech_flutter/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('App test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('Shop'), findsOneWidget);
   });
 }
